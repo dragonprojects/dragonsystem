3b70c602b0e3dfab66a7b6822bf3f9e5ec276a54
Jelle Z <jellezwiers@icloud.com>
2019-09-29 15:15:11 +0200

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-echo "Welcome to the Dragon OS installer for Ubuntu-based distributions. The installer will automatically start in a moment."
-sleep 10
-echo "YOU MUST KEEP THE FOLLOWING IN MIND:"
-echo "MAKE SURE YOU ARE USING THE NEWEST VERSION OF YOUR DISTRIBUTION!"
-echo "PLEASE MAKE SURE YOU ARE USING A STABLE INTERNET CONNECTION, PREFERABLY WIRED!"
-echo "NOBODY CAN BE HELD RESPONSIBLE FOR ANY DAMAGE TO YOUR SYSTEM!"
-echo "AGREE/ACCEPT SOFTWARE INSTALLATIONS WHEN PROMPTED!"
-echo "IF YOU WANT TO CANCEL THE INSTALLATION, TERMINATE THIS PROCESS NOW!"
-sleep 10
-echo "The installation of Dragon OS will now follow."
+echo "Welcome to the Dragon OS installer for the Ubuntu 18 Minimal ISO. The installer will automatically start in a moment."
+sleep 5
+echo "Using Dragon OS comes with no warrenty. Our source code can be viewed on https://github.com/jelle619/dragonos/."
+echo "To agree and start the installation of Dragon OS, enter your password."
 sleep 10
 
 
 echo "Preparing your system..."
 apt-get update -y
 apt-get upgrade -y
+
+echo "Installing required packages..."
+apt-get install -y flatpak
+
+echo "Removing unneeded packages..."
+apt autoremove --purge -y snapd
+
+echo "Setting up flatpak repositories.."
+flatpak remote-add fedora oci+https://registry.fedoraproject.org
