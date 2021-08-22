07e1b41f4957cebe827b6b58ae1be3d9ba0f14b5
Jelle Z <jellezwiers@icloud.com>
2021-08-22 23:20:12 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -39,7 +39,7 @@ flatpak install flathub org.gnome.eog -y # Eye of GNOME
 flatpak install flathub org.gnome.Evince -y # Evince
 flatpak install flathub org.libreoffice.LibreOffice -y # LibreOffice
 flatpak install flathub com.github.tchx84.Flatseal -y # Flatseal
-flatpak install flathub org.gnome.seahorse.Application # Seahorse
+flatpak install flathub org.gnome.seahorse.Application -y # Seahorse
 
 echo "Configuring installed software..."
 sed -i 's/managed=false/managed=true/g' /etc/NetworkManager/NetworkManager.conf # Set NetworkManager to manage networks
