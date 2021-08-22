e5d6cc45772fdbc2592cc06b0f85f9035b23c2e2
Jelle Z <jellezwiers@icloud.com>
2021-08-22 13:28:21 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ if [ "$EUID" -ne 0 ]
   then echo "Yikes! That didn't seem to work. To try again, restart the installer as the root user."
   exit
 fi
-whiptail --title "Dragon OS" --msgbox "Welcome to the Dragon System installer! By pressing OK, you agree to the terms and conditions and begin the installation." 8 78
+whiptail --title "Dragon System" --msgbox "Welcome to the Dragon System installer! By pressing OK, you agree to the terms and conditions and begin the installation." 8 78
 
 echo "Preparing your system..."
 apt-get update -y
@@ -15,7 +15,6 @@ apt-get install -y gdm3 gnome-session gnome-icon-theme --no-install-recommends #
 apt-get install -y xserver-xorg # X.Org Server
 apt-get install -y flatpak gnome-software-plugin-flatpak && flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo # Flatpak
 apt-get install -y network-manager network-manager-openvpn-gnome # Network manager for GNOME with OpenVPN support
-apt-get install -y unzip # Required for some applications to function properly
 apt-get install -y xdg-user-dirs # Application capable of setting up the default XDG home directories
 
 echo "Installing system applications..."
@@ -25,6 +24,7 @@ apt-get install -y nautilus
 apt-get install -y gnome-control-center
 apt-get install -y gnome-terminal
 apt-get install -y gnome-software
+apt-get install -y gnome-tweaks
 apt-get install -y software-properties-gtk
 
 # Installing additional applications...
@@ -32,8 +32,8 @@ flatpak install flathub org.gnome.FileRoller # File Roller
 flatpak install flathub org.mozilla.firefox -y # Firefox
 flatpak install flathub org.mozilla.Thunderbird -y # Thunderbird
 flatpak install flathub org.gnome.gedit -y # Gedit
+flatpak install flathub org.gnome.Calculator -y # Calculator
 flatpak install flathub ca.desrt.dconf-editor -y # Dconf
-flatpak install flathub org.gimp.GIMP -y # GIMP
 flatpak install flathub org.videolan.VLC -y # VLC
 flatpak install flathub org.gnome.eog -y # Eye of GNOME
 flatpak install flathub com.github.tchx84.Flatseal -y # Flatseal
